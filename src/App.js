c4d65697239b9bd6d2fad034ce5d9feb3a8151cf
TheBigStonk <maxfrancis212@gmail.com>
2020-05-11 23:00:34 +1200

    completed initial functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Flashcardlist from './Flashcardlist';
 import "./app.css"
 import axios from "axios"
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
-  
+  const [flashcards, setFlashcards] = useState([])
+  const [categories, setCategories] = useState([])
+
+  const categoryEl = useRef()
+  const amountEl = useRef()
+
   useEffect(() => {
-    axios
-     .get("https://opentdb.com/api.php?amount=10")
-     .then(res => {
-       setFlashcards(res.data.results.map((questionItem, index) => {
-        const answer = questionItem.correct_answer
-        const options = [
-           ...questionItem.incorrect_answers.map(a => decodeString(a)),
-           answer
-          ]
-        return {
-           id: `${index}-${Date.now()}`,
-           question: decodeString(questionItem.question),
-           answer: decodeString(questionItem.correct_answer),
-           options: options.sort(() => Math.random() - .5)
-         }
-       }))
-     })
+    axios.get("https://opentdb.com/api_category.php")
+    .then(res => {
+      setCategories(res.data.trivia_categories)
+    })
+  }, [])
+
+  useEffect(() => {
+
   }, [])
 
   function decodeString(str){
@@ -32,36 +27,61 @@ function App() {
     return textArea.value
   }
   
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios
+    .get("https://opentdb.com/api.php", {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+       const answer = questionItem.correct_answer
+       const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)),
+          answer
+         ]
+       return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: decodeString(questionItem.correct_answer),
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+    })
+  }
+
   return (
-    <div className="container">
-      <Flashcardlist flashcards={flashcards} />
-    </div>
+    <>
+      <form className = "header" onSubmit={handleSubmit}>
+        
+        <div className = "form-group">
+          <label htmlform="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => {
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
+        </div>
+      
+        <div className="form-group">
+          <label htmlform="amount">Number of Questions</label>  
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />   
+        </div>
+        
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+
+      </form>
+
+      <div className="container">
+        <Flashcardlist flashcards={flashcards} />
+      </div>
+    </>
   );
 }
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "What is 2+2",
-    answer: "4",
-    options: [
-      "2",
-      "3",
-      "4",
-      "5"
-    ]
-  },
-  {
-    id: 2,
-    question: "What is stonks?",
-    answer: "good",
-    options: [
-      "bad",
-      "good",
-      "suk",
-      "not good"
-    ]
-  }    
-]
 
 export default App;
