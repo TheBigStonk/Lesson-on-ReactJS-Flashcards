c0dfae7cb9722fca64f69488c67a26267230e19e
TheBigStonk <maxfrancis212@gmail.com>
2020-05-11 19:06:15 +1200

    Updates

---
+++ precache-manifest.63427d960e9f1ad828c6fe839612bc8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927f952446d5293db27c49d38c028684",
+    "url": "/StockFlash/index.html"
+  },
+  {
+    "revision": "8a4d3eb227fb3edb09a0",
+    "url": "/StockFlash/static/css/main.f8f11317.chunk.css"
+  },
+  {
+    "revision": "6534cb8499e126930e4d",
+    "url": "/StockFlash/static/js/2.5053fa0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StockFlash/static/js/2.5053fa0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4d3eb227fb3edb09a0",
+    "url": "/StockFlash/static/js/main.a5620444.chunk.js"
+  },
+  {
+    "revision": "b8fd202d02d7df86af11",
+    "url": "/StockFlash/static/js/runtime-main.c5137aee.js"
+  }
+]);
