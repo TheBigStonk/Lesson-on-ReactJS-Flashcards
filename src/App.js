01374d9158b114c3e80b3518abaac84a34436812
TheBigStonk <maxfrancis212@gmail.com>
2020-05-11 19:00:34 +1200

    make the stuff look nice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Flashcardlist from './Flashcardlist';
 import "./app.css"
+import axios from "axios"
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  
+  useEffect(() => {
+    axios
+     .get("https://opentdb.com/api.php?amount=10")
+     .then(res => {
+       setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = questionItem.correct_answer
+        const options = [
+           ...questionItem.incorrect_answers.map(a => decodeString(a)),
+           answer
+          ]
+        return {
+           id: `${index}-${Date.now()}`,
+           question: decodeString(questionItem.question),
+           answer: decodeString(questionItem.correct_answer),
+           options: options.sort(() => Math.random() - .5)
+         }
+       }))
+     })
+  }, [])
+
+  function decodeString(str){
+    const textArea = document.createElement("textarea")
+    textArea.innerHTML = str
+    return textArea.value
+  }
+  
   return (
+    <div className="container">
       <Flashcardlist flashcards={flashcards} />
+    </div>
   );
 }
 
